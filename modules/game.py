342c2125c2cabb471e2b256b9d848fe09e3f6837
doronharitan <doronharitan@gmail.com>
2019-03-30 22:07:39 +0300

    with plots and markers

--- modules/game.py
+++ modules/game.py
@@ -39,18 +39,21 @@ class GameModule(nn.Module):
         self.num_agents = num_agents # scalar: number of agents in this batch
         self.num_landmarks = num_landmarks # scalar: number of landmarks in this batch
         self.num_entities = self.num_agents + self.num_landmarks # type: int
-
+        self.world_dim = config.world_dim
         if self.using_cuda:
             self.Tensor = torch.cuda.FloatTensor
         else:
             self.Tensor = torch.FloatTensor
 
         locations = torch.rand(self.batch_size, self.num_entities, 2) * config.world_dim
-        # plt.plot(np.array(locations[1,:,0]), np.array(locations[1,:,1]), 'ro') ##
+        # plt.plot(np.array(locations[1,:,0]), np.array(locations[1,:,1]), 'ro') ## not using
 
         self.colors = (torch.rand(self.batch_size, self.num_entities, 1) * config.num_colors).floor()
         self.shapes = (torch.rand(self.batch_size, self.num_entities, 1) * config.num_shapes).floor()
-        Plot.save_step_plot(1, "start", locations, self.colors, self.shapes)
+
+
+        Plot.save_step_plot(self.batch_size, "start", locations, self.colors, self.shapes, self.world_dim)
+
 
 
         goal_agents = self.Tensor(self.batch_size, self.num_agents, 1)
@@ -125,7 +128,7 @@ class GameModule(nn.Module):
     """
     def forward(self, movements, goal_predictions, utterances, t):
         self.locations = self.locations + movements
-        Plot.save_step_plot(1, t, self.locations, self.colors, self.shapes)
+        Plot.save_step_plot(self.batch_size, t, self.locations, self.colors, self.shapes, self.world_dim)
         agent_baselines = self.locations[:, :self.num_agents]
         self.observations = self.locations.unsqueeze(1)- agent_baselines.unsqueeze(2)
         new_obs = self.goals[:,:,:2] - agent_baselines
--- modules/plot.py
+++ modules/plot.py
@@ -7,15 +7,18 @@ dict_shapes = {'[0.]': 'o', '[1.]': 'v'}
 class Plot:
 
     @staticmethod
-    def save_step_plot(batch_mum, iteration, locations, colors, shapes):
-        plt.clf()
-        marker = [dict_shapes[str(shape)] for shape in np.array(shapes[1])]
-        # plt.scatter(np.array(locations[1,:,0]), np.array(locations[1,:,1]) , color = [dict_colors[str(color)] for color in np.array(colors[1])]\
-        #          , marker = [dict_shapes[str(shape)] for shape in np.array(shapes[1])]) ##
-        # for value in dict_shapes:
-        #     idx_shape = marker[marker == value]
-        plt.scatter(locations[1,:,0].detach().numpy(), locations[1,:,1].detach().numpy() , color = [dict_colors[str(color)] for color in np.array(colors[1])] \
-                    , marker = 'o')
-        plt.axis([0, 16, 0, 16])
-        plt.show()
-        plt.savefig('plots\\batchnum_{0}iter_{1}.png'.format(batch_mum, iteration))
+    def save_step_plot(batch_num, iteration, locations, colors, shapes,world_dim):
+        for batch in range(batch_num):
+            plt.clf()
+            marker = np.array([dict_shapes[str(shape)] for shape in np.array(shapes[batch])])
+            for value in dict_shapes.values():
+                idx_shape = marker == value
+                locations_x = locations[batch,:,0].detach().numpy()[idx_shape]
+                locations_y = locations[batch,:,1].detach().numpy()[idx_shape]
+                colors_plot = np.array([dict_colors[str(color)] for color in np.array(colors[batch])])[idx_shape]
+                plt.scatter(locations_x, locations_y, color = colors_plot, marker = value)
+                plt.axis([0, world_dim, 0, world_dim])
+            # plt.show()
+            plt.savefig('plots\\batchnum_{0}iter_{1}.png'.format(batch, iteration))
+
+
