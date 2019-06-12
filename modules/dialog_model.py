bbee62fb02c5cd15b90a59959f5df5885e7dae84
Yael Green <yael@imubit.com>
2019-06-12 22:03:03 +0300

    fix: clean utterance network and DialogModel

--- modules/dialog_model.py
+++ modules/dialog_model.py
@@ -7,22 +7,13 @@
 An RNN based dialogue model. Performce both language and choice generation.
 """
 
-import sys
 import re
-import pdb
-import time
-import logging
-import numpy as np
 import torch
 import torch.nn as nn
-# import torch.nn.functional as F
 import torch.nn.init
 from torch.autograd import Variable
 import torch.nn.functional as F
 
-# from data import STOP_TOKENS
-# from domain import get_domain
-# from models import modules
 from modules import modules_for_lm
 
 
@@ -30,21 +21,11 @@ class DialogModel(modules_for_lm.CudaModule):
     def __init__(self, word_dict, item_dict, context_dict, output_length, config, device_id):
         super(DialogModel, self).__init__(device_id)
 
-        # domain = get_domain(args.domain)
-
         self.word_dict = word_dict
-        # self.item_dict = item_dict
-        # self.context_dict = context_dict
         self.config = config
 
         # embedding for words
         self.word_encoder = nn.Embedding(len(self.word_dict), config.nembed_word)
-
-        # # context encoder
-        # ctx_encoder_ty = modules_for_lm.RnnContextEncoder if args.rnn_ctx_encoder \
-        #     else modules_for_lm.MlpContextEncoder
-        # self.ctx_encoder = ctx_encoder_ty(len(self.context_dict), 3,
-        #     args.nembed_ctx, args.nhid_ctx, config.init_range, device_id)
 
         # a reader RNN, to encode words
         self.reader = nn.GRU(
@@ -66,34 +47,8 @@ class DialogModel(modules_for_lm.CudaModule):
 
         self.dropout = nn.Dropout(config.dropout)
 
-        # a bidirectional selection RNN
-        # it will go through input words and generate by the reader hidden states
-        # to produce a hidden representation
-        # self.sel_rnn = nn.GRU(
-        #     input_size=config.nhid_lang + config.nembed_word,
-        #     hidden_size=args.nhid_attn,
-        #     bias=True,
-        #     bidirectional=True)
-
         # mask for disabling special tokens when generating sentences
         self.special_token_mask = torch.FloatTensor(len(self.word_dict))
-
-        # # attention to combine selection hidden states
-        # self.attn = nn.Sequential(
-        #     torch.nn.Linear(2 * args.nhid_attn, args.nhid_attn),
-        #     nn.Tanh(),
-        #     torch.nn.Linear(args.nhid_attn, 1)
-        # )
-
-        # # selection encoder, takes attention output and context hidden and combines them
-        # self.sel_encoder = nn.Sequential(
-        #     torch.nn.Linear(2 * args.nhid_attn + args.nhid_ctx, args.nhid_sel),
-        #     nn.Tanh()
-        # )
-        # # selection decoders, one per each item
-        # self.sel_decoders = nn.ModuleList()
-        # for i in range(output_length):
-        #     self.sel_decoders.append(nn.Linear(args.nhid_sel, len(self.item_dict)))
 
         self.init_weights()
 
@@ -127,10 +82,6 @@ class DialogModel(modules_for_lm.CudaModule):
 
         self.word_encoder.weight.data.uniform_(-self.config.init_range, self.config.init_range)
 
-        # modules_for_lm.init_cont(self.attn, self.config.init_range)
-        # modules_for_lm.init_cont(self.sel_encoder, self.config.init_range)
-        # modules_for_lm.init_cont(self.sel_decoders, self.config.init_range)
-
     def read(self, inpt, lang_h, ctx_h, prefix_token="THEM:"):
         """Reads a given utterance."""
         # inpt contains the pronounced utterance
@@ -159,116 +110,6 @@ class DialogModel(modules_for_lm.CudaModule):
         result = self.to_device(result)
         return result
 
-    # def forward_selection(self, inpt, lang_h, ctx_h):
-    #     """Forwards selection pass."""
-    #     # run a birnn over the concatenation of the input embeddings and
-    #     # language model hidden states
-    #     inpt_emb = self.word_encoder(inpt)
-    #     h = torch.cat([lang_h, inpt_emb], 2)
-    #     h = self.dropout(h)
-    #
-    #     # runs selection rnn over the hidden state h
-    #     attn_h = self.zero_hid(h.size(1), self.args.nhid_attn, copies=2)
-    #
-    #     # compact weights to reduce memory usage
-    #     self.sel_rnn.flatten_parameters()
-    #     h, _ = self.sel_rnn(h, attn_h)
-    #
-    #     # perform attention
-    #     h = h.transpose(0, 1).contiguous()
-    #     logit = self.attn(h.view(-1, 2 * self.args.nhid_attn)).view(h.size(0), h.size(1))
-    #
-    #     prob = F.softmax(logit,dim=1).unsqueeze(2).expand_as(h)
-    #     attn = torch.sum(torch.mul(h, prob), 1, keepdim=True).transpose(0, 1).contiguous()
-    #
-    #     # concatenate attention and context hidden and pass it to the selection encoder
-    #     h = torch.cat([attn, ctx_h], 2).squeeze(0)
-    #     h = self.dropout(h)
-    #     h = self.sel_encoder.forward(h)
-    #
-    #     # generate logits for each item separately
-    #     outs = [decoder.forward(h) for decoder in self.sel_decoders]
-    #     return torch.cat(outs)
-
-    # def generate_choice_logits(self, inpt, lang_h, ctx_h):
-    #     """Similar to forward_selection, but is used while selfplaying.
-    #     Thus it is dealing with batches of size 1.
-    #     """
-    #     # run a birnn over the concatenation of the input embeddings and
-    #     # language model hidden states
-    #     inpt_emb = self.word_encoder(inpt)
-    #     h = torch.cat([lang_h.unsqueeze(1), inpt_emb], 2)
-    #     h = self.dropout(h)
-    #
-    #     # runs selection rnn over the hidden state h
-    #     attn_h = self.zero_hid(h.size(1), self.args.nhid_attn, copies=2)
-    #     self.sel_rnn.flatten_parameters()
-    #     h, _ = self.sel_rnn(h, attn_h)
-    #     h = h.squeeze(1)
-    #
-    #     # perform attention
-    #     logit = self.attn(h).squeeze(1)
-    #
-    #     prob = F.softmax(logit,dim=0).unsqueeze(1).expand_as(h)
-    #     attn = torch.sum(torch.mul(h, prob), 0, keepdim=True)
-    #
-    #     # concatenate attention and context hidden and pass it to the selection encoder
-    #     ctx_h = ctx_h.squeeze(1)
-    #     h = torch.cat([attn, ctx_h], 1)
-    #     h = self.sel_encoder.forward(h)
-    #
-    #     # generate logits for each item separately
-    #     logits = [decoder.forward(h).squeeze(0) for decoder in self.sel_decoders]
-    #     return logits
-
-    # def write_batch(self, bsz, lang_h, ctx_h, temperature, max_words=100):
-    #     """Generate sentenses for a batch simultaneously."""
-    #     eod = self.word_dict.get_idx('<selection>')
-    #
-    #     # resize the language hidden and context hidden states
-    #     lang_h = lang_h.squeeze(0).expand(bsz, lang_h.size(2))
-    #     ctx_h = ctx_h.squeeze(0).expand(bsz, ctx_h.size(2))
-    #
-    #     # start the conversation with 'YOU:'
-    #     inpt = torch.LongTensor(bsz).fill_(self.word_dict.get_idx('YOU:'))
-    #     inpt = Variable(self.to_device(inpt))
-    #
-    #     outs, lang_hs = [], [lang_h.unsqueeze(0)]
-    #     done = set()
-    #     # generate until max_words are generated, or all the dialogues are done
-    #     for _ in range(max_words):
-    #         # embed the input
-    #         inpt_emb = torch.cat([self.word_encoder(inpt), ctx_h], 1)
-    #         # pass it through the writer and get new hidden state
-    #         lang_h = self.writer(inpt_emb, lang_h)
-    #         out = self.decoder(lang_h)
-    #         # tie weights with encoder
-    #         scores = F.linear(out, self.word_encoder.weight).div(temperature)
-    #         # subtract max to make softmax more stable
-    #         scores.sub_(scores.max(1, keepdim=True)[0].expand(scores.size(0), scores.size(1)))
-    #         out = torch.multinomial(scores.exp(), 1).squeeze(1)
-    #         # save outputs and hidden states
-    #         outs.append(out.unsqueeze(0))
-    #         lang_hs.append(lang_h.unsqueeze(0))
-    #         inpt = out
-    #
-    #         data = out.data.cpu()
-    #         # check if all the dialogues in the batch are done
-    #         for i in range(bsz):
-    #             if data[i] == eod:
-    #                 done.add(i)
-    #         if len(done) == bsz:
-    #             break
-    #
-    #     # run it for the last word to get correct hidden states
-    #     inpt_emb = torch.cat([self.word_encoder(inpt), ctx_h], 1)
-    #     lang_h = self.writer(inpt_emb, lang_h)
-    #     lang_hs.append(lang_h.unsqueeze(0))
-    #
-    #     # concatenate outputs and hidden states into single tensors
-    #     return torch.cat(outs, 0), torch.cat(lang_hs, 0)
-
-    # special tokens for end of sentence or end of 'game'
     STOP_TOKENS = [
         '<eos>'
     ]
@@ -336,51 +177,6 @@ class DialogModel(modules_for_lm.CudaModule):
         lang_h = lang_h.unsqueeze(1)
 
         return logprobs, torch.cat(outs), lang_h, torch.cat(lang_hs)
-
-    # def score_sent(self, sent, lang_h, ctx_h, temperature):
-    #     """Computes likelihood of a given sentence."""
-    #     score = 0
-    #     # remove batch dimension from the language and context hidden states
-    #     lang_h = lang_h.squeeze(1)
-    #     ctx_h = ctx_h.squeeze(1)
-    #     inpt = Variable(torch.LongTensor(1))
-    #     inpt.data.fill_(self.word_dict.get_idx('YOU:'))
-    #     inpt = self.to_device(inpt)
-    #     lang_hs = []
-    #
-    #     for word in sent:
-    #         # add the context to the word embedding
-    #         inpt_emb = torch.cat([self.word_encoder(inpt), ctx_h], 1)
-    #         # update RNN state with last word
-    #         lang_h = self.writer(inpt_emb, lang_h)
-    #         lang_hs.append(lang_h)
-    #
-    #         # decode words using the inverse of the word embedding matrix
-    #         out = self.decoder(lang_h)
-    #         scores = F.linear(out, self.word_encoder.weight).div(temperature)
-    #         # subtract constant to avoid overflows in exponentiation
-    #         scores = scores.add(-scores.max().data[0]).squeeze(0)
-    #
-    #         mask = Variable(self.special_token_mask)
-    #         scores = scores.add(mask)
-    #
-    #         logprob = F.log_softmax(scores)
-    #         score += logprob[word[0]].data[0]
-    #         inpt = Variable(word)
-    #
-    #     # update the hidden state with the <eos> token
-    #     inpt_emb = torch.cat([self.word_encoder(inpt), ctx_h], 1)
-    #     lang_h = self.writer(inpt_emb, lang_h)
-    #     lang_hs.append(lang_h)
-    #
-    #     # add batch dimension back
-    #     lang_h = lang_h.unsqueeze(1)
-    #
-    #     return score, lang_h, torch.cat(lang_hs)
-
-    # def forward_context(self, ctx):
-    #     """Run context encoder."""
-    #     return self.ctx_encoder(ctx)
 
     def forward_lm(self, inpt, lang_h, ctx_h):
         """Run forward pass for language modeling."""
--- modules/utterance.py
+++ modules/utterance.py
@@ -21,9 +21,6 @@ class Utterance(nn.Module):
                     nn.Linear(action_processor_config.hidden_size, action_processor_config.vocab_size))
         self.gumbel_softmax = GumbelSoftmax(action_processor_config.use_cuda)
 
-        # self.args = {'init_range': 0.1, 'nhid_lang': 256, 'nembed_word': 10,
-        #          'nhid_ctx': 256, 'dropout': 0.5, 'momentum':0.1,
-        #          'lr':1, 'nesterov':True, 'clip':0.5, 'batch_size':512, 'temperature':0.5}
         self.dataset_dictionary = dataset_dictionary
         self.lm_model = DialogModel(dataset_dictionary.word_dict, None,
                                 None, 4,
@@ -44,20 +41,18 @@ class Utterance(nn.Module):
         self.decoder = nn.Linear(utterance_config.nhid_lang, utterance_config.nembed_word)
         self.config = utterance_config
 
-
-    def forward(self, processed, full_sentence,mode=None):
-
+    def forward(self, processed, full_sentence, mode=None):
         # perform forward for the language model
         utter = full_sentence.tolist()
         encoded_utter = np.array([self.dataset_dictionary.word_dict.w2i(utter[i].split(" "))
                                   for i in range(len(full_sentence))])
         encoded_pad = self.dataset_dictionary.word_dict.w2i(['<pad>'])
-        longst_sentence = len(max(encoded_utter,key=len))
+        longst_sentence = len(max(encoded_utter, key=len))
         encoded_utter = [encoded_utter[i] + encoded_pad * (longst_sentence - len(encoded_utter[i]))
                          if len(encoded_utter[i]) < longst_sentence else encoded_utter[i]
                          for i in range(len(full_sentence))]
         encoded_utter = Variable(torch.LongTensor(encoded_utter))
-        encoded_utter = encoded_utter.transpose(0,1)
+        encoded_utter = encoded_utter.transpose(0, 1)
 
         # create initial hidden state for the language rnn
         lang_h = self.lm_model.zero_hid(processed.size(0), self.lm_model.config.nhid_lang)
@@ -66,50 +61,24 @@ class Utterance(nn.Module):
         # remove batch dimension from the language and context hidden states
         lang_h = lang_h.squeeze(1)
 
-        # if we start a new sentence, prepend it with 'Hi'
-        inpt2 = Variable(torch.LongTensor(1, self.config.batch_size))
-        inpt2.data.fill_(self.dataset_dictionary.word_dict.get_idx('Hi'))
-
-        # max_words = 20
-        # temperature = 0.5
-        # for _ in range(max_words):
-        #     if inpt2 is not None:
-        #         # add the context to the word embedding
-        #         inpt_emb = torch.cat([self.word_encoder(inpt2), processed.unsqueeze(0)], 1).squeeze(0)
-        #         # update RNN state with last word
-        #         lang_h = self.writer(inpt_emb, lang_h)
-        #         lang_hs.append(lang_h)
+        # # if we start a new sentence, prepend it with 'Hi'
+        # inpt2 = Variable(torch.LongTensor(1, self.config.batch_size))
+        # inpt2.data.fill_(self.dataset_dictionary.word_dict.get_idx('Hi'))
 
         # decode words using the inverse of the word embedding matrix
-        out2 = self.decoder(lang_h)
-        scores = F.linear(out2, self.word_encoder.weight).div(self.config.temperature)
+        decoded_lang_h = self.decoder(lang_h)
+        scores = F.linear(decoded_lang_h, self.word_encoder.weight).div(self.config.temperature)
         # subtract constant to avoid overflows in exponentiation
         scores = scores.add(-scores.max().item()).squeeze(0)
 
-        # # disable special tokens from being generated in a normal turns
-        # if not resume:
-        #     mask = Variable(self.special_token_mask)
-        #     scores = scores.add(mask)
-
-        prob = F.softmax(scores, dim=2)  # TODO: is this th right dim?
-        logprob = F.log_softmax(scores, dim=2)  # TODO: is this th right dim?
-
-        # explicitly defining num_samples for pytorch 0.4.1
-        # word = prob.multinomial(num_samples=512).detach() #TODO: is this th right num_samples?
+        prob = F.softmax(scores, dim=2)
         word = torch.transpose(prob, 0, 1).multinomial(num_samples=DEFAULT_VOCAB_SIZE).detach()
-        # print(self.dataset_dictionary.word_dict.i2w(word[1,:]))
-        # logprob = logprob.gather(0, word)
-
-        # logprobs.append(logprob)
-        # outs.append(word.view(word.size()[0], 1))
-
         tgt = encoded_utter.reshape(encoded_utter.shape[0]*encoded_utter.shape[1])
-
-        loss = self.crit(out.view(-1, len(self.dataset_dictionary.word_dict)), tgt) # in FB code the inpt and tgt is one demintion less than the original data
+        # in FB code the inpt and tgt is one demintion less than the original data
+        loss = self.crit(out.view(-1, len(self.dataset_dictionary.word_dict)), tgt)
         if mode is None:
             self.opt.zero_grad()
-            # backward step with gradient clipping
-            # loss.backward()
+            # backward step with gradient clipping, use retain_graph=True
             loss.backward(retain_graph=True)
             torch.nn.utils.clip_grad_norm_(self.lm_model.parameters(),
                                            self.config.clip)
@@ -118,7 +87,6 @@ class Utterance(nn.Module):
         else:
             self.total_loss += loss
             return self.total_loss, word
-
 
     def create_utterance_using_old_code(self, training, processed):
         utter = self.utterance_chooser(processed)
